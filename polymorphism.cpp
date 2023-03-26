9919e6062e30a07919b4dc06a7b7d1a5baedb02e
Aryan Sharma <aryansharma89059@gmail.com>
2023-03-27 05:23:28 +0530

    polymorphism

---
+++ polymorphism.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+class animal{
+    public:
+    void animalsound()
+    {
+    cout<<"the animal has a peerticular sound";
+    }
+};
+class pig:public animal{
+    public:
+    void animalsound()
+    {
+        cout<<"the pig says wee wee\n\n";
+    }
+};
+
+class dog:public animal{
+    public:
+    void animalsound()
+    {
+        cout<<"the dog says bow bow"<<endl;
+
+    }
+};
+int main()
+{
+animal myanimal;
+pig mypig;
+dog mydog;
+
+myanimal.animalsound();
+mypig.animalsound();
+mydog.animalsound();
+return 0;
+}
